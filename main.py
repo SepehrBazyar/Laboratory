0578e69a3f8be0335994c4640990e34bd2535894
Reza Gh <r.msc.student93@gmail.com>
2021-05-26 21:42:36 +0430

    Revert "Menu items is updated"
    This reverts commit cf2d0b68436af6ba4be98549ceb9d85417470a5f.

--- main.py
+++ main.py
@@ -20,51 +20,17 @@ def hello_world():
 
 
 menu_dic = {
-    "name": "Main Menu",
-    "children": [
-        # This is for Login Menu
-        {"name": "Login",
-         "children": [
-             {"name": "Login as a Patient",
-              "description": "Login Patients",
-              "function": print("hello Patient")},
-             {"name": "Login as a Doctor",
-              "description": "Login Doctors",
-              "function": print("hello Doctor")},
-             {"name": "Login as a Operator",
-              "description": "Login Operators",
-              "function": print("hello Operator")},
-             {"name": "Login as a Managers",
-              "description": "Login Managers",
-              "function": print("hello Managers")},
-         ],
-         "description": "Login Users",
-         },
-
-        # This is for Register Menu
-        {"name": "Register",
-         "children": [
-             {"name": "Register as a Patient",
-              "description": "Register Patients",
-              "function": user_view.register},
-             {"name": "Register as a Doctor",
-              "description": "Register Doctors",
-              "function": user_view.register},
-             {"name": "Register as a Operator",
-              "description": "Register Operators",
-              "function": user_view.register},
-             {"name": "Register as a Managers",
-              "description": "Register Managers",
-              "function": user_view.register},
-         ],
-         "description": "Register Users",
-         },
-
-        # {
-        #     "name": "list users",
-        #     "description": "list users",
-        #     "function": user_view.repr_all_users
-        # }
+    "name": "main menu",
+    "children": [{
+        "name": "register",
+        "description": "register user",
+        "function": user_view.register
+    },
+        {
+            "name": "list users",
+            "description": "list users",
+            "function": user_view.repr_all_users
+        }
     ],
     "description": ""
 
