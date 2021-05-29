a64453b265e477666aff81b35390e849d9a2ce4c
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-29 17:18:16 +0430

    Write Logging for File Manager

--- core/manager.py
+++ core/manager.py
@@ -1,6 +1,13 @@
 import json
+# import psycopg2
+import logging
 
 from abc import ABC, abstractmethod
+from .exceptions import *
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)-10s - %(message)s')
 
 
 class BaseManager(ABC):
@@ -22,13 +29,15 @@ class FileManager(BaseManager):
     Managed Files to CRUD Data of Models.
     """
 
-    def __init__(self, name, path) -> None:
+    def __init__(self, name: str, path: str) -> None:
         self.file, self.path = name + 's.json', path
         try:
             with open(f".\\{self.path}\\{self.file}", 'x') as fl:
                 json.dump({}, fl, indent=4)
-        except:
-            pass
+            logging.info(f"{__name__}: {self.file} File Created in {path} Directory.")
+        except FileExistsError:
+            logging.warning(f"{__name__}: {self.file} File Existed in {path} Directory.")
+        logging.debug(f"{__name__}: File Manager Successfully.")
 
     def create(self, ID, instance) -> bool:
         """
@@ -39,7 +48,9 @@ class FileManager(BaseManager):
             models[ID] = instance.__dict__
             with open(f".\\{self.path}\\{self.file}", 'w') as fl:
                 json.dump(models, fl, indent=4)
+            logging.info(f"{__name__}: {ID} Object Created And Save to File.")
             return True
+        logging.error(f"{__name__}: {ID} Object Existed in the File.")
         return False
 
     def read(self, ID=None, attribute: str = None):
@@ -49,9 +60,12 @@ class FileManager(BaseManager):
         with open(f".\\{self.path}\\{self.file}", 'r') as fl:
             models = json.load(fl)
         if not ID:
+            logging.debug(f"{__name__}: Read And Pass All of the Objects.")
             return models
         if ID in models:
+            logging.debug(f"{__name__}: Read And Pass Attribute(s) {ID} Object.")
             return models[ID].get(attribute, models[ID])
+        logging.error(f"{__name__}: {ID} Not Existed Can't Read Anything.")
         return False
 
     def update(self, ID, attribute: str, new_value) -> bool:
@@ -63,7 +77,9 @@ class FileManager(BaseManager):
             models[ID].update({attribute: new_value})
             with open(f".\\{self.path}\\{self.file}", 'w') as fl:
                 json.dump(models, fl, indent=4)
+            logging.info(f"{__name__}: {ID}.{attribute} Updated to {new_value}.")
             return True
+        logging.error(f"{__name__}: {ID} Not Existed Can't Change Nothing.")
         return False
 
     def delete(self, ID) -> bool:
@@ -75,7 +91,9 @@ class FileManager(BaseManager):
             models.pop(ID)
             with open(f".\\{self.path}\\{self.file}", 'w') as fl:
                 json.dump(models, fl, indent=4)
+            logging.warning(f"{__name__}: {ID} Object Deleted Successfully.")
             return True
+        logging.error(f"{__name__}: {ID} Object Not Existed in the File.")
         return False
 
 
