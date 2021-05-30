fb6435ebf6c3623e28ea6bdfceef4b10f4fd8b91
mryazdan <mrrezayazdani@yahoo.com>
2021-05-31 01:22:16 +0430

    Add Basetest class

--- Lab/models.py
+++ Lab/models.py
@@ -5,5 +5,15 @@ from user.models import Patient
 from Lab.exceptions import *
 
 
-class Basetest:
-    pass
+class Basetest(BaseModels):
+    patients = Patient
+    estimate_time, result_time, create_time = datetime
+
+    def __init__(self, patient) -> None:
+        self.patient = patient
+        self.__result = None
+        self.create_time, self.estimate_time = datetime
+
+    def set_result(self, result):
+        self.result_time = datetime.now()
+        self.__result = result
