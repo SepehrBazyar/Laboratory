4f966c27dd56367136a4fe9b6f7a606499b78717
Reza Gh <r.msc.student93@gmail.com>
2021-06-01 01:20:17 +0430

    updating patient menu

---
+++ Lab/Lab_view.py
@@ -0,0 +1,32 @@
+from user import models
+from Lab.models import CV19
+from time import sleep
+
+Patient_list = []
+Tests = []
+
+
+def cv19_view():
+    print("Enter your profile : ")
+    first_name = input("First Name : ")
+    last_name = input("Last Name : ")
+    phone = input("Phone : ")
+    email = input("Email(Optional) : ")
+
+    patient = models.Patient(first_name, last_name, phone, email)
+    Patient_list.append(patient)
+
+    print("Your profile saved !")
+    print("Testing patient ", end="")
+
+    [sleep(1) or print(".", end="") for i in range(3)]
+
+    cv19 = CV19(patient)
+
+    print("Done ! ")
+
+    Tests.append(cv19)
+    print("Estimated result time : ", cv19.estimate_time)
+
+def result():
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import menu
 import user.menu_view as user_view
+import Lab.Lab_view as Lab_view
 from core.service import initialize_setup
 import core.utility as core_utility
 
@@ -25,44 +26,56 @@ menu_dic = {
         {"name": "Login",
          "authorization_function": core_utility.login_authorization,
          "children_with_authorization": [
-                  {"name": "patient",
-                    "children":[
-                        {"name": "new test",
-                            "function":user_view.repr_all_users,
-                            "description": "patient new test",
-                            },
-                    ],
-                    "description": "patient menu",
-                    },
-                    {"name": "doctor",
-                    "children":[
-                        {"name": "update test",
-                            "function":user_view.repr_all_users,
-                            "description": "doctor update test",
-                            },
-                    ],
-                    "description": "doctor menu",
-                    },
-                    {"name": "admin",
-                    "children":[
-                        {"name": "update test",
-                            "function":user_view.repr_all_users,
-                            "description": "doctor update test",
-                            },
-                    ],
-                    "description": "admin menu",
-                    },
-                    {"name": "manager",
-                    "children":[
-                        {"name": "update test",
-                            "function":user_view.repr_all_users,
-                            "description": "doctor update test",
-                            },
-                    ],
-                    "description": "manager menu",
-                    },
 
-              # todo: add all options for highest priority user(admin) as children and delete function
+             {"name": "patient",
+              "children": [
+                  {"name": "new test",
+                   "children": [
+                       {"name": "Covid19 test",
+                        "function": Lab_view.cv19_view,
+                        "description": "Covid19 test",
+                        },
+                   ],
+                   "description": "patient new test",
+                   },
+                  {"name": "result",
+                   "function": Lab_view.result,
+                   "description": "patient result",
+                   },
+              ],
+              "description": "patient menu",
+              },
+
+
+             {"name": "doctor",
+              "children": [
+                  {"name": "update test",
+                   "function": user_view.repr_all_users,
+                   "description": "doctor update test",
+                   },
+              ],
+              "description": "doctor menu",
+              },
+             {"name": "admin",
+              "children": [
+                  {"name": "update test",
+                   "function": user_view.repr_all_users,
+                   "description": "doctor update test",
+                   },
+              ],
+              "description": "admin menu",
+              },
+             {"name": "manager",
+              "children": [
+                  {"name": "update test",
+                   "function": user_view.repr_all_users,
+                   "description": "doctor update test",
+                   },
+              ],
+              "description": "manager menu",
+              },
+
+             # todo: add all options for highest priority user(admin) as children and delete function
          ],
          "description": "Login Users",
          },
