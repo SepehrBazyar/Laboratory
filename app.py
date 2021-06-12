d83f8e750f6961ae6cc6d44641e39429f02bea22
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-12 12:10:24 +0430

    change host to externally and index page register

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from user import html_view
 # config
 app = Flask(__name__)
 
+app.add_url_rule("/", 'register', html_view.register, methods=['GET', 'POST'])
 app.add_url_rule("/profile/<_id>", 'profile', html_view.profile)
 app.add_url_rule("/register", 'register',
                  html_view.register, methods=['GET', 'POST'])
 app.add_url_rule("/login", 'login', html_view.login, methods=['GET', 'POST'])
 
-app.run(port=8001)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001)
