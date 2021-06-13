592c88d27bc7c78603269da208a3ec19c30ec136
Reza Gh <r.msc.student93@gmail.com>
2021-06-13 20:52:02 +0430

    CV19 test class updated

--- lab/models.py
+++ lab/models.py
@@ -7,27 +7,27 @@ from lab.exceptions import *
 
 
 class Basetest(BaseModels):
-    patients: Patient
-    estimate_time: datetime
-    result_time: datetime
-    create_time: datetime
+    request_date: datetime
+    result_date: datetime
+    result: json
+    user_id: int
+    test_info_id: int
 
-    def __init__(self, patient) -> None:
-        self.patient = patient
-        self.__result = None
-        self.create_time, self.estimate_time = datetime
+    def __init__(self, request_date, result_date, user_id, test_info_id):
+        self.request_date = request_date
+        self.result_date = result_date
+        self.result = json.dumps({})
+        self.user_id = user_id
+        self.test_info_id = test_info_id
 
-    def set_result(self, result):
-        self.result_time = datetime.now()
-        self.__result = result
+    def get_result(self):
+        return self.result
 
 
 class CV19(Basetest):
 
-    def has_cv19(self):
-        if not hasattr(self, '__result'):
-            raise Exception
-        return bool(self.__result)
+    def __init__(self, request_date, result_date, user_id, test_info_id):
+        super().__init__(request_date, result_date, user_id, test_info_id)
 
 
 class TestInfo:
