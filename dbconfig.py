3477a18ed9fb685ed10358a16cbe07f78aef91c5
Reza Gh <r.msc.student93@gmail.com>
2021-06-13 18:57:49 +0430

    dbconfig updated

--- dbconfig.py
+++ dbconfig.py
@@ -1,4 +1,4 @@
 dbconfig = {
-            "dbname": 'LabDB', "host": 'localhost', "user": 'postgres', "password": '----!!'
-        }
+    "dbname": 'LabDB', "host": 'localhost', "user": 'postgres', "password": '----!!'
+}
 config = " ".join([f"{key}={dbconfig[key]}" for key in dbconfig.keys()])
