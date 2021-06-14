cfb349d511ea55f6ceeb12e2d761d05864e27732
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-14 16:43:43 +0430

    change reg page with base html

--- dbconfig.py
+++ dbconfig.py
@@ -1,4 +1,4 @@
 dbconfig = {
-    "dbname": 'LabDB', "host": 'localhost', "user": 'postgres', "password": '77829199'
+    "dbname": 'LabDB', "host": 'localhost', "user": 'postgres', "password": 'sepibzyr79'
 }
 config = " ".join([f"{key}={dbconfig[key]}" for key in dbconfig.keys()])
